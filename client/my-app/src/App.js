e6fb31546e687cf775a4bd7765d8f6f9c0644270
Dilshan samarasinghe <113366721+Dilshanjith@users.noreply.github.com>
2025-01-23 00:05:28 +0530

    update jsx file

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Home from './components/Home';
+import Edit from './components/Edit';
+import Create from './components/Create';
+import Read from './components/Read';
+
 
 function App(){
   return (
-    <div>App</div>
-  )
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/create" element={<Create />} />
+      <Route path="/read" element={<Read />} />
+      <Route path="/edit/:id" element={<Edit />} />
+    </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
